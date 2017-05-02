620ba587f30f2877e29244077c1ad498402cff0b
Yuta Koshizawa <koher@koherent.org>
2017-05-02 18:17:36 +0900

    Simplify tests

--- Tests/PromiseKTests/PromiseKTests.swift
+++ Tests/PromiseKTests/PromiseKTests.swift
@@ -3,45 +3,34 @@ import XCTest
 
 class PromiseKTests: XCTestCase {
     func testMap() {
-        let expectation = self.expectation(description: "")
-
-        _ = asyncGet(3).map {
-            XCTAssertEqual($0, 3)
-            return $0 * $0
-        }.map { (value: Int) in
-            XCTAssertEqual(value, 9)
-            expectation.fulfill()
+        let squared = asyncGet(3).map {
+            $0 * $0
         }
         
-        waitForExpectations(timeout: 3.0, handler: nil)
+        XCTAssertEqual(squared.sync(), 9)
     }
     
     func testFlatMap() {
         do {
-            let expectation = self.expectation(description: "")
-            
-            _ = asyncGet(3).flatMap { value -> Promise<()> in
-                XCTAssertEqual(value, 3)
-                expectation.fulfill()
-                return Promise<()>()
+            let a = asyncGet(2)
+            let b = asyncGet(3)
+            let sum = a.flatMap { a in
+                b.map { b in
+                    a + b
+                }
             }
             
-            waitForExpectations(timeout: 3.0, handler: nil)
+            XCTAssertEqual(sum.sync(), 5)
         }
         
         do {
-            let expectation = self.expectation(description: "")
-            
-            _ = asyncGet(3).flatMap { (value: Int) in
-                XCTAssertEqual(value, 3)
-                return asyncGet(value * value)
-            }.flatMap { (value: Int) -> Promise<()> in
-                XCTAssertEqual(value, 9)
-                expectation.fulfill()
-                return Promise<()>()
+            let sum = asyncGet(2).flatMap { a in
+                asyncGet(3).map { b in
+                    a + b
+                }
             }
             
-            waitForExpectations(timeout: 3.0, handler: nil)
+            XCTAssertEqual(sum.sync(), 5)
         }
     }
     
@@ -168,12 +157,12 @@ class PromiseKTests: XCTestCase {
             let expectation = self.expectation(description: "\(i)")
             
             let promise = Promise<Int> { resolve in
-                queue1.asyncAfter(deadline: DispatchTime.now() + Double(Int64(0.01 * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)) {
+                queue1.asyncAfter(deadline: .now() + 0.01) {
                     resolve(Promise(2))
                 }
             }
             
-            queue2.asyncAfter(deadline: DispatchTime.now() + Double(Int64(0.01 * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)) {
+            queue2.asyncAfter(deadline: .now() + 0.01) {
                 let _: Promise<Int> = promise.flatMap {
                     expectation.fulfill()
                     return Promise($0 * $0)
@@ -248,14 +237,6 @@ func asyncGetOrFail(_ value: Int, _ fails: Bool) -> Promise<() throws -> Int> {
 
 func asyncFailable(_ value: Int) -> Promise<() throws -> Int> {
     return asyncGetOrFail(value, arc4random() % 2 == 0)
-}
-
-func foo(_ a: Int, _ b: Int) -> (Int, Int) {
-    return (a, b)
-}
-
-func curry<A, B, Z>(_ f: @escaping (A, B) -> Z) -> (A) -> (B) -> Z {
-    return { a in { b in f(a, b) } }
 }
 
 struct FooError: Error {
