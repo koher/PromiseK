d9866a1c6e6b4ed4e253890d17133b82b3c37566
Yuta Koshizawa <koher@koherent.org>
2017-10-17 23:27:17 +0900

    Fix Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "PromiseK",
+    products: [
+        .library(name: "PromiseK", targets: ["PromiseK"]),
+    ],
     targets: [
         .target(name: "PromiseK", dependencies: []),
         .testTarget(name: "PromiseKTests", dependencies: ["PromiseK"]),
