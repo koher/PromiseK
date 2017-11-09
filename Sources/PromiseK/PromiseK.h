4af4397063ed09242b4990ec2034fed1b744ac87
Yuta Koshizawa <koher@koherent.org>
2017-11-10 00:48:41 +0900

    Add the xcodeproj for iOS
    It can be installed by Carthage.

---
+++ Sources/PromiseK/PromiseK.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for PromiseK.
+FOUNDATION_EXPORT double PromiseKVersionNumber;
+
+//! Project version string for PromiseK.
+FOUNDATION_EXPORT const unsigned char PromiseKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PromiseK/PublicHeader.h>
+
+
